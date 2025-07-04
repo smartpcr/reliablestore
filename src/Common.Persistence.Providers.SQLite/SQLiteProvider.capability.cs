19c7de26e819c01075546759d3be53f919a4ee26
xiaodoli <xiaodoli@microsoft.com>
2025-07-03 17:45:59 -0700

    sqlite

---
+++ src/Common.Persistence.Providers.SQLite/SQLiteProvider.capability.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SQLiteProvider.capability.cs" company="Microsoft Corp.">
+//     Copyright (c) Microsoft Corp. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Common.Persistence.Providers.SQLite
+{
+    public class SQLiteProvider_capability
+    {
+        
+    }
+}
